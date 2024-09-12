85b2deb14db87624e40f163d87ddc0a36ca603b4
Sia <sryu5320@stu.d214.org>
2024-09-12 11:15:58 -0500

    I change the data type of the task and change the initial value in the array to Task objects instead of Strings and in the list add the .nameOfTask property.

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tasks = ["Mow the Lawn","Feed the Dog","Grocery Shop"]
+    @State var tasks: [Task] = [Task(nameOfTask: "Mow the Lawn"),Task(nameOfTask:"Feed the Dog") , Task(nameOfTask: "Grocery Shop")]
     var body: some View {
         List(tasks, id: \.self){task in
-            Text(task)
+            Text(task.nameOfTask)
         }
     }
 }
