89ba5206682fc184d2eeef7ec0662b54145bfac1
Max Droll <mdroll6088@stu.d214.org>
2024-09-16 11:03:32 -0500

    made the HeaderView now use an alert to add the info

--- HeaderView.swift
+++ HeaderView.swift
@@ -11,21 +11,27 @@ struct HeaderView: View{
     @Binding var tasks: [Task]
     @State var storedString = ""
     @State var storedTime = ""
+    @State var showAlert = false
     var body: some View{
         HStack{
-            VStack{
+            Button(action: {
+                showAlert = true
+            }, label: {
+                Text("Add Task")
+                    .frame(width: 200, height: 100)
+                    .background(.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                    .foregroundStyle(.white)
+            })
+            .alert("Make a Task", isPresented: $showAlert, actions: {
                 TextField("Enter a Task", text: $storedString)
-                    .textFieldStyle(.roundedBorder)
                 TextField("Enter a Time", text: $storedTime)
-                    .textFieldStyle(.roundedBorder)
-            }
-            Button("Add Task") {
-                tasks.append(Task(nameOfTask: storedString , timeEstimate: storedTime))
-            }
-            .frame(width: 200, height: 100)
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 25.0))
-            .foregroundStyle(.white)
+                Button("Add it", role: .cancel, action: {
+                    tasks.append(Task(nameOfTask: storedString , timeEstimate: storedTime))
+                })
+            }, message: {
+            Text("Please Make a Task")
+            })
         }
     }
 }
